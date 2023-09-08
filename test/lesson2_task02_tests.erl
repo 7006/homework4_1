10a6f9c98d845ae8fd4023ea57efaa1562ece653
7006 <22399553+7006@users.noreply.github.com>
2023-09-08 10:10:02 +0300

    Update lesson2_task02_tests.erl

--- test/lesson2_task02_tests.erl
+++ test/lesson2_task02_tests.erl
@@ -21,21 +21,21 @@ but_last_test_() ->
         {
             "it should find 2 last elements for the two-element list",
             ?_assertEqual(
-                {a, b},
+                [a, b],
                 lesson2_task02:but_last([a, b])
             )
         },
         {
             "it should find 2 last elements for the three-element list",
             ?_assertEqual(
-                {b, c},
+                [b, c],
                 lesson2_task02:but_last([a, b, c])
             )
         },
         {
             "it should find 2 last elements for a non-empty list",
             ?_assertEqual(
-                {e, f},
+                [e, f],
                 lesson2_task02:but_last([a, b, c, d, e, f])
             )
         }
